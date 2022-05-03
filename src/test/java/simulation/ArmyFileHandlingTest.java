ee6d375fa5968fd6e58f362a08a026ddf918b0b5
Eirik Elvestad <eirik.org@gmail.com>
2022-05-03 21:05:05 +0200

    feat: make testers for ArmyFileHandling

---
+++ src/test/java/simulation/ArmyFileHandlingTest.java
@@ -0,0 +1,126 @@
+package simulation;
+
+import edu.ntnu.idatt2001.eirielv.Units.*;
+import edu.ntnu.idatt2001.eirielv.simulation.*;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArmyFileHandlingTest {
+
+    @Nested
+    class armyCSVInput_works_as_expected_when_it{
+
+        @Test
+        public void reads_correct_information_from_a_csv_file_and_makes_a_army(){
+            Army armyOne = new Army("HumanArmy");
+            ArmyFileHandling armyFileHandling = new ArmyFileHandling();
+            List<Unit> expectedInformation = new ArrayList<>();
+
+            expectedInformation.add(new InfantryUnit("Footman",100));
+            expectedInformation.add(new InfantryUnit("Footman",100));
+            expectedInformation.add(new CommanderUnit("Mountain King",180));
+            armyOne.addAll(expectedInformation);
+
+            assertEquals(armyOne.getAllUnits().toString(), armyFileHandling.getArmyFromCSVInput("HumanArmy").getAllUnits().toString()); ;
+
+        }
+
+        @Test void does_not_read_incorrect_information_from_a_csv_file(){
+            Army armyOne = new Army("WrongInformation");
+            ArmyFileHandling armyFileHandling = new ArmyFileHandling();
+            try{
+                armyFileHandling.getArmyFromCSVInput("WrongInformation");
+            }
+            catch (IllegalArgumentException e) {
+                assertTrue(true);
+            }
+        }
+    }
+
+    @Nested
+    class unitsToCSVFile_works_as_expected_when_it{
+
+        @Test
+        public void throws_illegalArgumentException_if_there_are_special_characters_in_file_name(){
+            Army specialCharacterArmy = new Army("Eirik's army!");
+            ArmyFileHandling armyFileHandling = new ArmyFileHandling();
+            List<Unit> units = new ArrayList<>();
+            try {
+                units.add(new CavalryUnit("Knight", 15));
+                units.add(new RangedUnit("Archer", 20));
+                units.add(new InfantryUnit("Solider1", 30));
+                units.add(new CommanderUnit("Rex", 40));
+                specialCharacterArmy.addAll(units);
+                armyFileHandling.writeUnitsToCSVFile(specialCharacterArmy);
+            }
+            catch(IllegalArgumentException e){
+                assertTrue(true);
+            }
+        }
+
+        @Test
+        public void throws_IllegalArgumentException_if_file_already_exists()  {
+            Army testArmyDuplicate = new Army("testArmyDuplicate");
+            ArmyFileHandling armyFileHandling = new ArmyFileHandling();
+            List<Unit> units = new ArrayList<>();
+            File testFile = null;
+            try {
+                units.add(new CavalryUnit("Knight", 15));
+                units.add(new RangedUnit("Archer", 20));
+                units.add(new InfantryUnit("Solider1", 30));
+                units.add(new CommanderUnit("Rex", 40));
+                testArmyDuplicate.addAll(units);
+                armyFileHandling.writeUnitsToCSVFile(testArmyDuplicate);
+                armyFileHandling.writeUnitsToCSVFile(testArmyDuplicate);
+            }
+            catch(IllegalArgumentException e){
+                assertTrue(true);
+            }
+            testFile = new File("src/main/resources/ArmyCSVFiles/testArmyDuplicate.csv");
+            testFile.delete();
+        }
+
+        @Test
+        public void makes_a_CSV_file(){
+            Army armyOne = new Army("armyOne");
+            ArmyFileHandling armyFileHandling = new ArmyFileHandling();
+
+            List<Unit> units = new ArrayList<>();
+            units.add(new CavalryUnit("Knight", 15));
+            units.add(new RangedUnit("Archer", 20));
+            units.add(new InfantryUnit("Soldier1", 30));
+            units.add(new CommanderUnit("Rex", 40));
+            armyOne.addAll(units);
+            armyFileHandling.writeUnitsToCSVFile(armyOne);
+            File testFile = new File("src/main/resources/ArmyCSVFiles/armyOne.csv");
+            assertTrue(testFile.exists());
+            testFile.delete();
+        }
+
+        @Test
+        public void stores_correct_information_to_a_CSV_file(){
+            Army armyOne = new Army("armyOne");
+            ArmyFileHandling armyFileHandling = new ArmyFileHandling();
+
+            List<Unit> expectedUnits = new ArrayList<>();
+            expectedUnits.add(new CavalryUnit("Knight", 15));
+            expectedUnits.add(new RangedUnit("Archer", 20));
+            expectedUnits.add(new InfantryUnit("Soldier1", 30));
+            expectedUnits.add(new CommanderUnit("Rex", 40));
+            armyOne.addAll(expectedUnits);
+            armyFileHandling.writeUnitsToCSVFile(armyOne);
+
+            Army armyTwo = armyFileHandling.getArmyFromCSVInput("armyOne");
+            assertEquals(armyOne.toString(), armyTwo.toString());
+            File testFile = new File("src/main/resources/ArmyCSVFiles/armyOne.csv");
+            testFile.delete();
+        }
+    }
+}
