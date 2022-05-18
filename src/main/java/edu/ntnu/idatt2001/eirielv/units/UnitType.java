ce441f87b3e67f95140e148115bfe86b50b8ca6e
Eirik Elvestad <eirik.org@gmail.com>
2022-05-19 01:53:49 +0200

    feat: add javaDoc to UnitType

--- src/main/java/edu/ntnu/idatt2001/eirielv/units/UnitType.java
+++ src/main/java/edu/ntnu/idatt2001/eirielv/units/UnitType.java
@@ -1,5 +1,9 @@
 package edu.ntnu.idatt2001.eirielv.units;
 
+/**
+ * @author Eirik Elvestad
+ * UnitType is a enum class that holds the different types of units
+ */
 public enum UnitType {
     CAVALRYUNIT,
     COMMANDERUNIT,
