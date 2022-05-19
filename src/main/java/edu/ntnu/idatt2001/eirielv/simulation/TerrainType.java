b84a9396b74dd530102156debb57b6fc4a717658
Eirik Elvestad <eirik.org@gmail.com>
2022-05-19 19:30:22 +0200

    feat: add TerrainType enum

---
+++ src/main/java/edu/ntnu/idatt2001/eirielv/simulation/TerrainType.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2001.eirielv.simulation;
+
+/**
+ * @author Eirik Elvestad
+ * TerrainType is an enum class that holds all types of tarrains
+ */
+public enum TerrainType {
+
+    FOREST,
+    HILL,
+    PLAINS
+}
