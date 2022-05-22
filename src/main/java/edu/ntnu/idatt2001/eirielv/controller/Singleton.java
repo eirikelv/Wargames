12a2243f182087a5cbf056909d65eafdf8d5f9f0
Eirik Elvestad <eirik.org@gmail.com>
2022-05-23 01:13:20 +0200

    feat: make Singleton return army and armyname

--- src/main/java/edu/ntnu/idatt2001/eirielv/controller/Singleton.java
+++ src/main/java/edu/ntnu/idatt2001/eirielv/controller/Singleton.java
@@ -1,13 +1,16 @@
 package edu.ntnu.idatt2001.eirielv.controller;
 
+import edu.ntnu.idatt2001.eirielv.simulation.Army;
+
 /**
  * Singleton.java is a class that makes information pass from one controller to another.
  * This method is retrieved from GeeksForGeeks, 21.05.2022 from @
  * <a href="https://www.geeksforgeeks.org/singleton-design-pattern/">GeeksForGeeks</a>
  */
 public class Singleton {
-    private String armyName1;
-    private String armyName2;
+    private Army army1;
+    private Army army2;
+
 
     private static Singleton obj;
 
@@ -21,18 +24,26 @@ public class Singleton {
     }
 
     public String getArmyName1() {
-        return armyName1;
+        return army1.getName();
     }
 
     public String getArmyName2() {
-        return armyName2;
+        return army2.getName();
     }
 
-    public void setArmyName1(String armyName1) {
-        this.armyName1 = armyName1;
+    public Army getArmy1(){
+        return army1;
     }
 
-    public void setArmyName2(String armyName2) {
-        this.armyName2 = armyName2;
+    public Army getArmy2(){
+        return army2;
+    }
+
+    public void setArmy1(Army army1){
+        this.army1 = army1;
+    }
+
+    public void setArmy2(Army army2){
+        this.army2 = army2;
     }
 }
