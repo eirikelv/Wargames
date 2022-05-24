c61fcd9f89ff4fffb3fb234c9df8c81dbb57588c
Eirik Elvestad <eirik.org@gmail.com>
2022-05-24 05:48:34 +0200

    refactor: merge AddUnitsController1.java and AddUnitsController2.java to one class

--- src/main/java/edu/ntnu/idatt2001/eirielv/controller/AddUnitsController.java
+++ src/main/java/edu/ntnu/idatt2001/eirielv/controller/AddUnitsController.java
@@ -8,32 +8,37 @@ import edu.ntnu.idatt2001.eirielv.units.UnitType;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.util.Objects;
+
 /**
- * This class is a controller for {@link SwitchScene} AddUnitsWindow1.fxml, and adds a new unit to an army
+ * AddUnitsController is a controller for {@link SwitchScene} AddUnitsWindow.fxml, and adds a new unit to an army
  * @author Eirik Elvestad
  */
-public class AddUnitsController1 {
+public class AddUnitsController {
 
     @FXML
     public TableView<Unit> tableViewArmy;
+
     @FXML
     public ChoiceBox<UnitType> checkBoxUnitType = new ChoiceBox<>();
+
     @FXML
     public TextField textFieldUnitName;
     @FXML
     public TextField textFieldHealth;
     @FXML
     public TextField textFieldQuantity;
+
     @FXML
     public Button buttonAddUnit;
 
     /**
-     * Initializes the controller and sets value to checkbox and tableView
+     * Initializes the controller and sets value to checkbox and tableView and uses setArmyToEdit() to decide
+     * which army from {@link Singleton} to store in TableView
      */
     @FXML
     public void initialize() {
-        Army army1 = Singleton.getInstance().getArmy1();
-        TableDecorator.fillTable(3, army1, tableViewArmy);
+        TableDecorator.fillTable(3, setArmyToEdit(), tableViewArmy);
         checkBoxUnitType.getItems().add(0, UnitType.INFANTRYUNIT);
         checkBoxUnitType.getItems().add(1, UnitType.CAVALRYUNIT);
         checkBoxUnitType.getItems().add(2, UnitType.COMMANDERUNIT);
@@ -52,16 +57,16 @@ public class AddUnitsController1 {
         if(!textFieldQuantity.hasProperties()) throw new IllegalArgumentException("Type in quantity of units");
         try {
             ArmyFileHandling armyFileHandler = new ArmyFileHandling();
-            Army csvArmy1;
+            Army csvArmy;
             UnitType unitType = findUnitType();
             String name = textFieldUnitName.getText();
             int health = Integer.parseInt(textFieldHealth.getText());
             int quantity = Integer.parseInt(textFieldQuantity.getText());
 
-            csvArmy1 = armyFileHandler.getArmyFromCSVInput(Singleton.getInstance().getArmyName1());
-            csvArmy1.addAll(UnitFactory.addDuplicateUnitsAsList(unitType, name, health, quantity));
-            armyFileHandler.writeUnitsToCSVFile(csvArmy1);
-            TableDecorator.fillTable(3, csvArmy1, tableViewArmy);
+            csvArmy = armyFileHandler.getArmyFromCSVInput(setArmyToEdit().getName());
+            csvArmy.addAll(UnitFactory.addDuplicateUnitsAsList(unitType, name, health, quantity));
+            armyFileHandler.writeUnitsToCSVFile(csvArmy);
+            TableDecorator.fillTable(3, csvArmy, tableViewArmy);
 
         }catch (Exception e){
             AlertBox.alertError(e.getMessage());
@@ -75,25 +80,33 @@ public class AddUnitsController1 {
     }
 
     /**
-     * This method finds the unitType of the new unit. The user uses a checkbox to set the uniType of the unit
-     * @return the correct unitType represented ass UnitType
+     * findUnitType() takes inn user input from checkBoxUnitType and decides what unit type the new
+     * unit is
+     * @return returns the unit type for the new unit, represented as UnitType
      */
-    public UnitType findUnitType() {
+    public UnitType findUnitType(){
         int index = checkBoxUnitType.getSelectionModel().getSelectedIndex();
-        UnitType unitType = null;
-        switch (index) {
-            case 0:
-                unitType = UnitType.INFANTRYUNIT;
-                break;
-            case 1:
-                unitType = UnitType.CAVALRYUNIT;
-                break;
-            case 2:
-                unitType = UnitType.COMMANDERUNIT;
-                break;
-            case 3:
-                unitType = UnitType.RANGEDUNIT;
+        return switch (index) {
+            case 0 -> UnitType.INFANTRYUNIT;
+            case 1 -> UnitType.CAVALRYUNIT;
+            case 2 -> UnitType.COMMANDERUNIT;
+            case 3 -> UnitType.RANGEDUNIT;
+            default -> null;
+        };
+    }
+
+
+    /**
+     * setArmyToEdit method gets the buttonID to button stored in {@link Singleton}, which was
+     * the last button pressed before changing scene, and decides what army in {@link Singleton}
+     * to store the new army.
+     * @return {@link Singleton} army1 or army2 represented as Army
+     */
+    public Army setArmyToEdit(){
+        if(Objects.equals(Singleton.getInstance().getButton().getId(), "addUnits1")){
+            return Singleton.getInstance().getArmy1();
+        }else{
+            return Singleton.getInstance().getArmy2();
         }
-        return unitType;
     }
 }
