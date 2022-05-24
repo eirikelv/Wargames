d961aa26957067e8f208d493b377953c7ef53ec4
Eirik Elvestad <eirik.org@gmail.com>
2022-05-24 07:33:00 +0200

    doc: add extra JavaDoc to CavalryUnit.java

--- src/main/java/edu/ntnu/idatt2001/eirielv/model/units/CavalryUnit.java
+++ src/main/java/edu/ntnu/idatt2001/eirielv/model/units/CavalryUnit.java
@@ -1,10 +1,10 @@
-package edu.ntnu.idatt2001.eirielv.units;
+package edu.ntnu.idatt2001.eirielv.model.units;
 
-import edu.ntnu.idatt2001.eirielv.simulation.TerrainType;
-import edu.ntnu.idatt2001.eirielv.simulation.Unit;
+import edu.ntnu.idatt2001.eirielv.model.simulation.TerrainType;
+import edu.ntnu.idatt2001.eirielv.model.simulation.Unit;
 
 /**
- * CavalryUnit klass represents the cavalry unit. The cavalry unit has a charge attack and melee attack.
+ * CavalryUnit class represents the cavalry unit. The cavalry unit has a charge attack and melee attack.
  * @author Eirik Elvestad
  */
 public class CavalryUnit extends Unit {
@@ -12,7 +12,8 @@ public class CavalryUnit extends Unit {
     private int attackedCount = 0; // how many times the unit has been attacked
 
     /**
-     * This is the constructor for the CavalryUnit class that defines how a cavalry unit should look and its variables
+     * This is the constructor for the CavalryUnit class that defines how a cavalry unit should look and its variables.
+     * It also set the UnitType to CavalryUnit
      * @param name   The String name is a short descriptive name of the cavalry unit
      * @param health The int health is a value that describes the cavalry unit's health
      * @param attack The int attack represents the units weapon
@@ -25,7 +26,8 @@ public class CavalryUnit extends Unit {
 
     /**
      *This is the construct for cavalry Unit with set variables attack = 20 and armor = 12. This constructor will be used
-     * to call cavalry unit with set values attack and armor
+     * to call cavalry unit with set values attack and armor.
+     * It also set the UnitType to CavalryUnit.
      * @param name Ths String name is a short descriptive name of the cavalry unit
      * @param health The int health is the value that describes the cavalry unit's health
      */
@@ -39,7 +41,7 @@ public class CavalryUnit extends Unit {
      * The cavalry unit has an advantage because it has extra damage with first attack, and because of that, the cavalry
      * unit gets 6 in attack bonus with the first attack. After that the attackbonus equals 2
      * attack bonus to illustrate this.
-     * TODO write Java doc for TerrainType
+     * The cavalry unit also have a benefit in plains, where it gets extra attack bonus.
      * @return Returns the attack bonus to the cavalry unit
      */
     @Override
@@ -56,8 +58,8 @@ public class CavalryUnit extends Unit {
 
     /**
      *{@inheritDoc}
-     * TODO write Java doc for TerrainType
-     * The cavalry unit has a low resistance bonus. To illustrate this, the cavalry unit gets 1 in resistance bonus
+     * The cavalry unit has a low resistance bonus. To illustrate this, the cavalry unit gets 1 in resistance bonus.
+     * The cavalry unit also has a disadvantage in forest, where the resist bonus is 0
      * @return Returns the resistance bonus to the cavalry unit
      */
     @Override
