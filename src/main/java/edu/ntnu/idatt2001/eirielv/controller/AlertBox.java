6ecbd7d1da6653c1a408fa882aeae36e98559251
Eirik Elvestad <eirik.org@gmail.com>
2022-05-24 05:54:30 +0200

    doc: add JavaDoc to AlertBox.java

--- src/main/java/edu/ntnu/idatt2001/eirielv/controller/AlertBox.java
+++ src/main/java/edu/ntnu/idatt2001/eirielv/controller/AlertBox.java
@@ -1,40 +1,36 @@
 package edu.ntnu.idatt2001.eirielv.controller;
 
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
+import edu.ntnu.idatt2001.eirielv.simulation.Army;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.atomic.AtomicInteger;
-
+/**
+ * AlertBox makes alerterror and informationbox for representing winner of simulation
+ * @author Eirik Elvestad
+ */
 public class AlertBox {
 
     /**
-     * The method provides a general error alert box which can be specified with the display message
+     * alertError method makes a general error alert box which takes in and display message
      * @param message the display message to the alert box
      */
     public static void alertError(String message) {
-        Alert dateAlert = new Alert(Alert.AlertType.ERROR);
-        dateAlert.setTitle("Error");
-        dateAlert.setContentText(message);
-        dateAlert.showAndWait();
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setTitle("Error");
+        errorAlert.setContentText(message);
+        errorAlert.showAndWait();
     }
 
     /**
-     * This method provides the user with helpful information during submission or creation
-     * @param message popup message
+     * winnerBox method represents the winner in the simulation
+     * @param army is the winning army
+     * @param message is the representation of the army
      */
-    public static void confirmBox(String message){
-        Alert dateAlert = new Alert(Alert.AlertType.CONFIRMATION);
-        dateAlert.setTitle("Confirmation");
-        dateAlert.setContentText(message);
-        dateAlert.showAndWait();
+    public static void winnerBox(Army army, String message){
+        Alert winnerInformation = new Alert(Alert.AlertType.INFORMATION);
+        winnerInformation.setTitle("WINNER");
+        winnerInformation.setHeaderText(army.getName());
+        winnerInformation.setContentText(message);
+        winnerInformation.show();
     }
 
 }
