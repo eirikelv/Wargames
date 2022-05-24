d9ee8f1b4ec99c0e0cb877082e712a2eaef9386c
Eirik Elvestad <eirik.org@gmail.com>
2022-05-24 13:27:55 +0200

    feat: add information on TutorialPage

--- src/main/java/edu/ntnu/idatt2001/eirielv/controller/TutorialController.java
+++ src/main/java/edu/ntnu/idatt2001/eirielv/controller/TutorialController.java
@@ -1,4 +1,32 @@
 package edu.ntnu.idatt2001.eirielv.controller;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import java.io.IOException;
+
+/**
+ * TutorialController is the controller to {@link SwitchScene} TutorialPage.fxml and shows the tutorial
+ * to the program
+ * @author Eirik Elvestad
+ */
 public class TutorialController {
+
+    /**
+     *initializes the controller
+     */
+    public void initialize() {
+
+    }
+
+    /**
+     * goToFrontPage takes the user to {@link SwitchScene} FrontPage.fxml
+     * @param event checks if the button "goToFrontPage" is pressed by the mouse
+     * @throws IOException if the new scene has invalid information
+     */
+    @FXML
+    public void goToFrontPage(MouseEvent event) throws IOException {
+        SwitchScene.switchScene("FrontPage",event);
+    }
+
 }
