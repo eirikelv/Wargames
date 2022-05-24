79c834b9ba481f3acffe9159f3e8fa0829d03938
Eirik Elvestad <eirik.org@gmail.com>
2022-05-24 06:43:59 +0200

    doc: add javaDoc to WargamesGUI.java

--- src/main/java/edu/ntnu/idatt2001/eirielv/controller/WargamesGUI.java
+++ src/main/java/edu/ntnu/idatt2001/eirielv/controller/WargamesGUI.java
@@ -5,20 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * WarGamesGUI starts the application
+ * @author Eirik Elvestad
+ */
 public class WargamesGUI extends Application {
 
     private static Stage stage;
 
+    /**
+     * Method for launching the application
+     * @param args
+     */
     public static void main(String[] args){
         launch(args);
     }
 
     /**
+     * The start method starts the first stage, and sets the size of it
      * @param stage
-     * @throws Exception
      */
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         try {
             this.stage=stage;
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/edu/ntnu/idatt2001/eirielv/view/FrontPage.fxml"));
@@ -31,16 +39,28 @@ public class WargamesGUI extends Application {
         }
     }
 
+    /**
+     * init decides what the application should do before scene start
+     * @throws Exception if there is a fault before startup
+     */
     @Override
     public void init() throws Exception {
         super.init();
     }
 
+    /**
+     * stop decides the program should do while exiting the application
+     * @throws Exception if there is a fault in the closing stage
+     */
     @Override
     public void stop() throws Exception{
         super.stop();
     }
 
+    /**
+     * getStage returns the stage the application is on
+     * @return stage represented as Stage
+     */
     public static Stage getStage(){
         return stage;
     }
